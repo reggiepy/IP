932d099d8e2ccc925b9a51cdade15c1e79d7640f
wangtong <wangtongpy@foxmail.com>
2019-04-29 15:57:03 +0800

    初始化项目

---
+++ config.py
@@ -0,0 +1,5 @@
+# coding=utf-8
+
+# author: Reggie
+# time:   2019/04/29 14:30
+
---
+++ utils.py
@@ -0,0 +1,12 @@
+# coding=utf-8
+
+# author: Reggie
+# time:   2019/04/29 14:29
+import requests
+
+def check_proxy():
+    requests.get()
+
+
+if __name__ == '__main__':
+    pass
